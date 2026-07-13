b432635c66b5e0749aa7bf113e46417c42c2885c
agungrio307 <agungrio307@gmail.com>
2026-07-13 16:57:02 +0700

    ename pattern printing exercises example

--- src/topic02_loops/PatternPrintingExercises.java
+++ src/topic02_loops/PatternPrintingExercises.java
@@ -1,6 +1,6 @@
 package topic02_loops;
 
-public class PerulanganPola {
+public class PatternPrintingExercises {
     public static void main(String[] args) {
         // Soal 1 
         System.out.println("Soal 1");
